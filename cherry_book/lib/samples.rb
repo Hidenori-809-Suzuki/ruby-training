8d26345a6c1270307caabc7ea1a45fa383ea027d
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2023-04-11 03:15:53 +0900

    move_file_ci

---
+++ cherry_book/lib/samples.rb
@@ -0,0 +1,22 @@
+dimensions = [
+  # [縦, 横]
+  [10, 20],
+  [30, 40],
+  [50, 60],
+]
+# 面積の計算結果を格納する配列
+areas = []
+
+# ブロック引数が1個であれば、ブロック引数の値が配列になる
+dimensions.each do |dimension|
+  length = dimension[0]
+  width = dimension[1]
+  areas << length * width
+end
+areas  #=> [200,1200,3000]
+
+# 配列の要素分だけブロック引数を用意すると、各要素の値が別々の変数に格納される
+dimensions.each do |length, width|
+  areas << length * width
+end
+areas  #=> [200,1200,3000]
